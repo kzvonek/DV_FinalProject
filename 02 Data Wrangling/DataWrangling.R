30d32d2d87defa6c58d54a060b4ba41629bc2901
Evan Johnston <evanjohnston@Evans-MacBook-Pro.local>
2015-05-11 08:35:49 -0500

    added visualizations

--- 02 Data Wrangling/DataWrangling.R
+++ 02 Data Wrangling/DataWrangling.R
@@ -1,9 +1,9 @@
 dfSTATEHOUSINGTOTALS<-left_join(dfSTATETOTALS, dfSTATEHOUSING, by = "STATE")
-dfSTATEHOUSINGTOTALS<- dfSTATEHOUSINGTOTALS %>% mutate(EMP_RATE=EMPLOYMENT/POPULATION) %>% mutate(WATER_PERC=WATERAREA/TOTALAREA) %>% mutate(LAND_PERC=LANDAREA/TOTALAREA) %>% mutate(AVG_EMP=EMPLOYMENT/NUM_ESTABLISHMENTS) %>% mutate(AVG_PAY=PAYROLL/POPULATION)
+dfSTATEHOUSINGTOTALS<- dfSTATEHOUSINGTOTALS %>% mutate(EMP_RATE=EMPLOYMENT/POPULATION) %>% mutate(WATER_PERC=WATERAREA/TOTALAREA) %>% mutate(LAND_PERC=LANDAREA/TOTALAREA) %>% mutate(AVG_EMP=EMPLOYMENT/NUM_ESTABLISHMENTS) %>% mutate(AVG_PAY=PAYROLL/EMPLOYMENT)
 dfSTATECOMPGROWTH <- dfTOPCOMPANIES %>% group_by(STATE) %>% summarise (groAvg = mean(GROWTH), revAvg = mean(REVENUE))
 dfTOTALS<- left_join(dfSTATEHOUSINGTOTALS, dfSTATECOMPGROWTH, by="STATE")
-dfNormalized <- dfTOTALS %>% mutate(VarianceEMP_RATE=(EMP_RATE-mean(EMP_RATE))) %>% mutate(VarianceAVG_EMP=(AVG_EMP-mean(AVG_EMP))) %>% mutate(VarianceAVG_PAY=(AVG_PAY-mean(AVG_PAY))) %>% mutate(VarianceGro_Avg=(groAvg-mean(groAvg))) %>% mutate(VarianceRevAvg=(revAvg-mean(revAvg)))
-dfNormalized <- dfNormalized %>% select(VarianceAVG_EMP, VarianceAVG_PAY, VarianceGro_Avg)
+dfNormalized <- dfTOTALS %>% mutate(normalEMP_RATE=((EMP_RATE-mean(EMP_RATE))/(sd(EMP_RATE)))) %>% mutate(normalAVG_EMP=((AVG_EMP-mean(AVG_EMP))/(sd(AVG_EMP)))) %>% mutate(normalAVG_PAY=((AVG_PAY-mean(AVG_PAY))/(sd(AVG_PAY)))) %>% mutate(normalGro_Avg=((groAvg-mean(groAvg))/(sd(groAvg))))
+dfNormalized <- dfNormalized %>% select(normalEMP_RATE, normalAVG_EMP, normalAVG_PAY, normalGro_Avg)
 
 dfSTATEHOUSINGTOTALS1<-left_join(dfSTATETOTALS, dfSTATEHOUSING, by = "STATE")
 dfSTATEHOUSINGTOTALS1<-transform(dfSTATEHOUSINGTOTALS1, NUM_ESTABLISHMENTS = as.numeric(as.character(NUM_ESTABLISHMENTS)))
--- 03 Visualizations/viz3.R
+++
@@ -1,1 +0,0 @@
-viz3 <- stateGrowth %>% mutate(groPerc=cume_dist(groMax), revPerc=cume_dist(revMax)) %>% ggplot(aes(x=groPerc, y=revPerc, color=STATE)) + geom_point(size=8)
